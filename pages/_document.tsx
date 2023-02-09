5c2d63d9b9390043c8b434bc5dc44106eac00aa6
jiwooproity <jiwooproity@naver.com>
2023-02-10 03:24:50 +0900

    Axios Config 테스트 / Main 화면 구성

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { Navbar } from "@/components";
 
 export default function Document() {
   return (
@@ -9,9 +8,9 @@ export default function Document() {
         <meta name="description" content="나의 포트폴리오 소개 웹" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link href="//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css" rel="stylesheet" type="text/css"></link>
       </Head>
       <body>
-        <Navbar />
         <Main />
         <NextScript />
       </body>
