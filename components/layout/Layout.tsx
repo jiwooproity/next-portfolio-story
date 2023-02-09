39fd8298eccdd85baba2ccffb9d9f2da94730e8d
jiwooproity <jiwooproity@naver.com>
2023-02-10 03:45:30 +0900

    Layout Component - Head 정보 통합

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Container from "../common/Container";
 import Footer from "../common/Footer";
 import Navbar from "../common/Navbar";
@@ -9,6 +10,13 @@ type LayoutProps = {
 const Layout = ({ children }: LayoutProps) => {
   return (
     <>
+      <Head>
+        <title>Next Portfolio Web</title>
+        <meta name="description" content="나의 포트폴리오 소개 웹" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <link href="//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css" rel="stylesheet" type="text/css"></link>
+      </Head>
       <Navbar />
       <Container>{children}</Container>
       <Footer />
