b544b96d95e7917841f88589d265b5b6a290ac7f
sojiwoo <58384366+jiwooproity@users.noreply.github.com>
2023-02-10 16:51:23 +0900

    Portfolio Page - 노션 데이터베이스 연동 및 데이터 표기, 섬네일 등록 ( Next/Image 확인 필요 )

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["localhost", "s3.us-west-2.amazonaws.com", "*"],
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
