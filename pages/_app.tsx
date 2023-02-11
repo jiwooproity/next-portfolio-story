4cbc2ebd370f268f776bd2ea682293794a76d95b
jiwooproity <jiwooproity@naver.com>
2023-02-11 17:54:31 +0900

    Notion Sort 정렬 Body 파라미터 추가, SCSS 모듈 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,5 @@
 import type { AppProps } from "next/app";
-
-import "@/styles/globals.css";
-import "@/styles/common/navbar.css";
-import "@/styles/common/footer.css";
-import "@/styles/common/container.css";
-
-import "@/styles/home.css";
-import "@/styles/portfolio.css";
+import "@/styles/globals.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
