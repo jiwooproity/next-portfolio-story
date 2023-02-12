e49f6154821d95656ee9bfb96c37e4d636b5630a
jiwooproity <jiwooproity@naver.com>
2023-02-12 19:34:13 +0900

    GIF 용량 수정

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    i18n,
     domains: ["www.notion.so", "notion.so", "images.unsplash.com", "s3.us-west-2.amazonaws.com"],
     format: ["image/png", "image/webp", "image/jpeg", "image/gif"],
   },
